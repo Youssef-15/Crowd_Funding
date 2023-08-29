99eae0f075cfe914b73404045b4ebc5a1ff8a6ad
Youssef Gamaleldin <youssefjamaleldin15@gmail.com>
2023-08-29 17:36:32 +0300

    CRUD

--- #OOP Project.py
+++ #OOP Project.py
@@ -1,5 +1,6 @@
 import re
 import maskpass
+import tkinter
 
 print("----------Registration----------")
 email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
@@ -9,24 +10,19 @@ mobile_regexUS = re.compile(r"^\d{10}")
 mobile_regexSp = re.compile(r"^[6,7]\d{8}")
 mobile_regexIt = re.compile(r"^[3]\d{9}")
 pass_regex = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$")
+file1 = open("New.txt", "a")
 class Register:
     def signup(self):
         #First and last name
         while True:
             first_name = input("First Name: ")
             if first_name.isalpha():
-                file1 = open("Data.txt", "a")
-                file1.write(first_name+"\n")
-                file1.close()
                 break
             else:
                 print("Invalid First Name")
         while True:
             last_name = input("Last Name: ")
             if last_name.isalpha():
-                file1 = open("Data.txt", "w")
-                file1.write(last_name+"\n")
-                file1.close()
                 break
             else:
                 print("Invalid last Name")
@@ -34,9 +30,6 @@ class Register:
         while True:
             email = input("E-mail: ")
             if re.fullmatch(email_regex, email):
-                file1 = open("Data.txt", "a")
-                file1.write(email+"\n")
-                file1.close()
                 break
             else:
                 print("Invalid Email")
@@ -50,9 +43,6 @@ class Register:
             print("------------------------------------------------------")
             password = maskpass.askpass()
             if pass_regex.match(password):
-                file1 = open("Data.txt", "a")
-                file1.write(password+"\n")
-                file1.close()
                 break
             else:
                 if len(password) < 6:
@@ -75,63 +65,55 @@ class Register:
                 print("Doesn't Match with Password..!\nPlease, Reconfirm Your Password: ")
         #Mobile Phone
         while True:
-            countries = ["Egypt", "England", "USA", "Spain", "Italy"]
+            countries = ["1", "2", "3", "4", "5"]
             country = input("Choose a number to select Your Country: \n1-Egypt\n2-England\n3-USA\n4-Spain\n5-Ita1ly\n")
             if country not in countries:
                 print("Invalid Input..!\n Please Choose your country from the list: ")
             else:
                 #Egypt
-                if country == "Egypt":
-                    file1 = open("Data.txt", "a")
-                    file1.write(country+"\n")
-                    file1.close()
+                if country == "1":
                     mobile_phone = input("Enter your phone number: +20")
                     if mobile_regexEg.match(mobile_phone):
+                        print("Welcome", first_name, last_name, "from Egypt..!")
                         break
                     else:
                         print("Invalid Phone Number..!\nRe-Enter a valid number: ")
                 #England
-                elif country == "England":
-                    file1 = open("Data.txt", "a")
-                    file1.write(country+"\n")
-                    file1.close()
+                elif country == "2":
                     mobile_phone = input("Enter your phone number: +44")
                     if mobile_regexEn.match(mobile_phone):
+                        print("Welcome", first_name, last_name, "from England..!")
                         break
                     else:
                         print("Invalid Phone Number..!\nRe-Enter a valid number: ")
                 #USA
-                elif country == "USA":
-                    file1 = open("Data.txt", "a")
-                    file1.write(country+"\n")
-                    file1.close()
+                elif country == "3":
                     mobile_phone = input("Enter your phone number: +1")
                     if mobile_regexUS.match(mobile_phone):
+                        print("Welcome", first_name, last_name, "from USA..!")
                         break
                     else:
                         print("Invalid Phone Number..!\nRe-Enter a valid number: ")
                 #Spain
-                elif country == "Spain":
-                    file1 = open("Data.txt", "a")
-                    file1.write(country+"\n")
-                    file1.close()
+                elif country == "4":
                     mobile_phone = input("Enter your phone number: +34")
                     if mobile_regexSp.match(mobile_phone):
+                        print("Welcome", first_name, last_name, "from Spain..!")
                         break
                     else:
                         print("Invalid Phone Number..!\nRe-Enter a valid number: ")
-                #France
-                elif country == "Italy":
-                    file1 = open("Data.txt", "a")
-                    file1.write(country+"\n")
-                    file1.close()
+                #Italy
+                elif country == "5":
                     mobile_phone = input("Enter your phone number: +39")
                     if mobile_regexIt.match(mobile_phone):
+                        print("Welcome", first_name, last_name, "from Italy..!")
                         break
                     else:
                         print("Invalid Phone Number..!\nRe-Enter a valid number: ")
-                    
-        print("Welcome", first_name, last_name, "from", country+"..!")
+        info = "".join(["First Name: ",first_name,"\n","Last Name: ", last_name,"\n", "Email: ", email,"\n", "Password: ", password,"\n", "Country: ", country])
+        info=info+"\n"
+        file1.write(info)
+        file1.close()
     def signin(self):
         while True:
             email = input("Please, Enter Your Email: \n")
@@ -156,22 +138,22 @@ class Register:
                     print("Password doesn't contain any small letter")
                 elif not re.search(r'[@$!%*#?&]', password):
                     print("Password doesn't contain any special character")
-        readfile = open("Data.txt")
-        data = readfile.readlines()
+        readfile = open("New.txt", "r")
+        #file1 = readfile.readlines()
         users = []
-        for i in data:
-            users.append(i.strip("\n"))
-        print(users)
         for user in users:
             userdetails = user.split(",")
-            if userdetails[1] == email and userdetails[2] == password:
+            if userdetails[2] == email and userdetails[3] == password:
                 print("Logged in Successfully")
-                mainMenu.projectmenu(email)
                 readfile.close()
                 break
         else:
-            print("User Doesnt Exit")
-            self.login()
+            print("User Doesn't Exist")
+            self.signin()
+        for i in readfile:
+            users.append(i.strip("\n"))
+        print(users)
+
 registration = Register()
 registration.signup()
 x = input("Do you want to signin?![y/n]")
---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+class Project:
+    title = ""
+    details = ""
+    total_target = 0
+    start_time = "01/01/2001"
+    end_time = "01/01/2001"
+
+class DD (Project):
+    def ss():
+        details = "one two three four five"
+
+pp = DD()
+pp.ss()
+print(pp.details)
